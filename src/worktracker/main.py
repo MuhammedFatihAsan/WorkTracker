7953caf834736d584229ebed66e94e3a90ed4203
MuhammedFatihAsan <m.fatihasan@hotmail.com>
2025-08-12 14:27:44 +0300

    .

--- src/worktracker/main.py
+++ src/worktracker/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from .api.routers import users_router, tasks_router
 
 #FastAPI uygulamasını başlat
 app = FastAPI(title="WorkTracker")
@@ -6,4 +7,10 @@ app = FastAPI(title="WorkTracker")
 #HealthPoint
 @app.get("/health")
 def health_check():
-    return {"status": "ok"} 
+    return {"status": "ok"}
+
+# ÖNEMLİ: Router'ları uygulamaya ekle
+# /users ile başlayan uçlar için users_router
+app.include_router(users_router)
+# /tasks ile başlayan uçlar için tasks_router
+app.include_router(tasks_router)
